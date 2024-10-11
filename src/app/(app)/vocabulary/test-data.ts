156e810b0a61dbab122f04fd15de1542caf8a3ff
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-10-11 11:47:36 +0700

    feat: Add test data for vocabulary sets

---
+++ src/app/(app)/vocabulary/test-data.ts
@@ -0,0 +1,129 @@
+const vocabularyData = [
+    {
+        categoryTitle: "Từ vựng CEFR",
+        vocabularies: [
+            {
+                title: "Pre Beginner",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "A0",
+                progress: "Cấp độ A0",
+                completed: 0,
+                total: 166,
+                timeLeft: 0,
+                crown: false, // Không có crown
+            },
+            {
+                title: "Beginner",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "A1",
+                progress: "Cấp độ A1",
+                completed: 10,
+                total: 768,
+                timeLeft: 5,
+                crown: true, // Có crown
+            },
+            {
+                title: "Pre Intermediate",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "A2",
+                progress: "Cấp độ A2",
+                completed: 0,
+                total: 1430,
+                timeLeft: 0,
+                crown: false, // Không có crown
+            },
+            {
+                title: "Intermediate",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "B1",
+                progress: "Cấp độ B1",
+                completed: 500,
+                total: 1700,
+                timeLeft: 50,
+                crown: true, // Có crown
+            },
+            {
+                title: "Upper Intermediate",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "B2",
+                progress: "Cấp độ B2",
+                completed: 0,
+                total: 2000,
+                timeLeft: 0,
+                crown: false, // Không có crown
+            },
+            {
+                title: "Advanced",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "C1",
+                progress: "Cấp độ C1",
+                completed: 1000,
+                total: 2500,
+                timeLeft: 100,
+                crown: true, // Có crown
+            },
+        ],
+    },
+    {
+        categoryTitle: "Từ vựng thông dụng",
+        vocabularies: [
+            {
+                title: "Common Vocabularies",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "Thông dụng",
+                progress: "Cấp độ thông dụng",
+                completed: 500,
+                total: 1606,
+                timeLeft: 200,
+                crown: true, // Có crown
+            },
+            {
+                title: "Everyday Words",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "Thông dụng",
+                progress: "Cấp độ thông dụng",
+                completed: 300,
+                total: 1200,
+                timeLeft: 100,
+                crown: false, // Không có crown
+            },
+        ],
+    },
+    {
+        categoryTitle: "Từ vựng TOEIC",
+        vocabularies: [
+            {
+                title: "TOEIC Vocabulary",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "TOEIC",
+                progress: "Cấp độ TOEIC",
+                completed: 0,
+                total: 1200,
+                timeLeft: 0,
+                crown: false, // Không có crown
+            },
+            {
+                title: "TOEIC Advanced",
+                thumbnail:
+                    "https://voca-land.sgp1.cdn.digitaloceanspaces.com/-1/1653745898655/630aba7dd711828c5f4dfc994ec966d95a0993a1d4a39c319e06c8ce6a2fcfcd.png",
+                level: "Advanced TOEIC",
+                progress: "Cấp độ TOEIC nâng cao",
+                completed: 600,
+                total: 1500,
+                timeLeft: 60,
+                crown: true, // Có crown
+            },
+        ],
+    },
+];
+
+export default vocabularyData;

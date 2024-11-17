66a356f68aea9626a892473f9e2a467078534020
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-17 15:50:15 +0700

    refactor: clean up layout component structure for improved readability

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,41 +2,27 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Badge } from "@/components/ui/badge";
 import SidebarApp from "./sidebar-app";
 
-
-
-
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-
-        <div
-            className={` antialiased bg-[#f4f7fc]`}
-        >
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                enableSystem
-                disableTransitionOnChange
-            >
-                <div className="lg:hidden flex flex-col items-center justify-center h-screen">
-                    <Badge className="bg-primary">Mobile currently unavailable</Badge>
-                    <div>Please open this page on desktop</div>
-                </div>
-                <div
-                    className={
-                        "hidden lg:flex flex-col h-screen w-full md:flex-row dark:bg-[#171717] overflow-hidden"
-                    }
-                >
-                    <SidebarApp />
-                    <div className="w-full p-8 overflow-y-auto scrollbar-default  dark:scrollbar-dark  ">
-                        {children}
-                    </div>
-                </div>
-            </ThemeProvider>
+  return (
+    <div className={` antialiased bg-[#f4f7fc]`}>
+      <div className="lg:hidden flex flex-col items-center justify-center h-screen">
+        <Badge className="bg-primary">Mobile currently unavailable</Badge>
+        <div>Please open this page on desktop</div>
+      </div>
+      <div
+        className={
+          "hidden lg:flex flex-col h-screen w-full md:flex-row dark:bg-[#171717] overflow-hidden"
+        }
+      >
+        <SidebarApp />
+        <div className="w-full p-8 overflow-y-auto scrollbar-default  dark:scrollbar-dark  ">
+          {children}
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }

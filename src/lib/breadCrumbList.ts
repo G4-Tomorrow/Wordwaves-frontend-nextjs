135511a07af95107dc4497dc1e9393e10fd47b54
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-11-19 17:13:25 +0700

    Add Breadcrumb component with custom separator and breadcrumb list for admin navigation

---
+++ src/lib/breadCrumbList.ts
@@ -0,0 +1,51 @@
+const breadCrumbList = [
+    {
+        title: "Admin",
+        url: "/admin",
+    },
+    {
+        title: "Users",
+        url: "/admin/users",
+    },
+
+    {
+        title: "Vocabulary",
+        url: "/admin/vocabulary",
+    },
+
+    {
+        title: "Add Collection",
+        url: "/admin/collections/new",
+    },
+    // {
+    //     title: "Edit Collection",
+    //     url: "/admin/collections/[collectionId]/edit",
+    // },
+    // {
+    //     title: "Topics",
+    //     url: "/admin/topics",
+    // },
+    {
+        title: "Add Topic",
+        url: "/admin/topics/new",
+    },
+    // {
+    //     title: "Edit Topic",
+    //     url: "/admin/topics/[topicId]/edit",
+    // },
+
+    // {
+    //     title: "User Details",
+    //     url: "/admin/users/[userId]",
+    // },
+    {
+        title: "Add Words",
+        url: "/admin/words/new",
+    },
+    // {
+    //     title: "Word Details",
+    //     url: "/admin/words/[wordName]",
+    // },
+];
+
+export default breadCrumbList;
